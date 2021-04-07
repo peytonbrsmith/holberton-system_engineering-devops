d3d94e71f62ef8f3aeaebbc1830d5b0ac051687c
peytonbrsmith <peytonbrsmith@gmail.com>
2021-04-07 14:50:54 -0500

    updated adv

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,23 +1,16 @@
-# Install nginx listening on port 80, static html = Holberton School, perm redirect 301
-
+# Installs nginx and configures it with redirect
 package { 'nginx':
-  ensure         => 'installed',
-  package_source => 'nginx-mainline'
+  ensure => installed,
 }
 
-file { 'index.html':
-  path    => '/var/www/html/index.html',
-  ensure  => present,
-  content => 'Holberton School'
+file_line { 'redir':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'server_name _;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
-file { 'custom_404.html':
-  path    => '/var/www/html/custom_404.html',
-  ensure  => present,
-  content => "Ceci n'est pas une page"
-}
-
-exec { 'sed1':
-  path    => '/usr/bin/env:/usr/bin:/usr/sbin:/bin',
-  command => 'sed -r -i "/^\s+server_name .+;/a\ \tlocation /redirect_me {\n\t\treturn 301 https:\/\/www.youtube.com\/watch\?v=QH2-TGUlwu4;\n\t}" /etc/nginx/sites-available/default'
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }
