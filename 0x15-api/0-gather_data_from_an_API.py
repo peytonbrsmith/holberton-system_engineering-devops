63881169b2a439a2ff9c997d94670e3a86f1e7d1
peytonbrsmith <peytonbrsmith@gmail.com>
2021-05-03 20:28:41 +0000

    added task 0 and 1

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+"""
+Write a Python script that, using this REST API,
+for a given employee ID,
+returns information about his/her TODO list progress
+"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    eid = sys.argv[1]
+
+    user = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(eid))
+    tasks = requests.get("https://jsonplaceholder.typicode.com/todos/").json()
+    total = 0
+    done = 0
+    completed = []
+    for task in tasks:
+        userId = task.get('userId')
+        if userId == int(eid):
+            total = total + 1
+            if task.get('completed') is True:
+                done = done + 1
+                completed.append(task.get('title'))
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.json().get('name'), done, total
+    ))
+    for task in completed:
+        print("\t {}".format(task))
--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+"""
+Using what you did in the task #0, extend your
+Python script to export data in the CSV format.
+"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+    import csv
+
+    eid = sys.argv[1]
+
+    user = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(eid))
+    tasks = requests.get("https://jsonplaceholder.typicode.com/todos/").json()
+    etasks = []
+    for task in tasks:
+        userId = task.get('userId')
+        if userId == int(eid):
+            info = []
+            info.append(eid)
+            info.append(user.json().get('name'))
+            info.append(str(task.get('completed')))
+            info.append(task.get('title'))
+            etasks.append(info)
+
+    with open('{}.csv'.format(eid), 'w') as f:
+        write = csv.writer(f, quoting=csv.QUOTE_ALL)
+        write.writerows(etasks)
